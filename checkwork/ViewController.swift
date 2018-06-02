23caa8395eb27fd7d53b05e210bbf4c4e8ebb7f4
rxlk012178 <yuichoko.19820125@ezweb.ne.jp>
2018-06-02 19:42:04 +0900

    first commit

--- checkwork/ViewController.swift
+++ checkwork/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Double = 0.0
+    var numberf: Int = 0
+    var numbers: Int = 0
+    @IBOutlet var labelf: UILabel!
+    @IBOutlet var labels: UILabel!
+    @IBOutlet var Countlabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +26,42 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plusf() {
+        numberf = numberf + 1
+        labelf.text = String(numberf)
+    }
+    
+    @IBAction func minusf() {
+        numberf = numberf - 1
+        labelf.text = String(numberf)
+    }
+    
+    @IBAction func pluss() {
+        numbers = numbers + 1
+        labels.text = String(numbers)
+    }
+    
+    @IBAction func minuss() {
+        numbers = numbers - 1
+        labels.text = String(numbers)
+    }
+    
+    @IBAction func counting() {
+        number = (Double(numberf) / ( Double((numberf + numbers)))) * 100
+        print(number)
+        Countlabel.text = String(number)
+    }
+
+    @IBAction func reset() {
+        number = 0
+        numberf = 0
+        numbers = 0
+        Countlabel.text = String(number)
+        labelf.text = String(numberf)
+        labels.text = String(numbers)
+        
     }
 
 
