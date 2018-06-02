7c464cec5a9e58086da612c220108dc9ee6d1640
rxlk012178 <yuichoko.19820125@ezweb.ne.jp>
2018-06-02 16:12:02 +0900

    Initial Commit

---
+++ checkwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checkwork
+//
+//  Created by 松田結衣 on 2018/06/02.
+//  Copyright © 2018年 YuiMatsuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
